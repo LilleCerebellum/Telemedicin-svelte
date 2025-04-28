63d67ea664a822d86905ca7a03c9cf5414307390
LilleCerebellum <Soellingvraa72@gmail.com>
2025-04-28 06:37:51 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { goto } from "$app/navigation";
+
+    let brugernavn = $state("");
+    let password = $state("");
+
+    const loginButton = () => {
+        if (brugernavn === "Morten" && password === "Morten") {
+            goto('/home')
+
+        } else if (brugernavn === "Rasmus" && password === "Rasmus") {
+            window.open("https://maps.app.goo.gl/qcViKmE1Rud1AVjr9")
+        }
+        
+        else { alert("nuhuh") } 
+    }
+</script>
+
+<input class="input m-2" til bind:value={brugernavn} placeholder="Brugernavn">
+<input class="input m-2" til bind:value={password} placeholder="Password">
+<button class="btn btn-primary m-2" onclick={loginButton}> Login </button>
